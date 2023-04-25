1d447dfe4d2c85a09cfefbc005e14fc49c775afd
h.sadeghi <hoj.sadeghi@gmail.com>
2023-04-25 19:43:27 +0330

    First commit, Just adding some config

---
+++ src/main/java/ir/expense/tracker/makharej/MakharejApplication.java
@@ -0,0 +1,13 @@
+package ir.expense.tracker.makharej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakharejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakharejApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/expense/tracker/makharej/MakharejApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.expense.tracker.makharej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakharejApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
